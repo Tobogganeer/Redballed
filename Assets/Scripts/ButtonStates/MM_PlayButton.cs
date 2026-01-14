56830895738793d2abcd4bd3643140bfd1737289
Osama Hussein <osamahusseinn2@gmail.com>
2026-01-14 09:06:41 -0500

    Added main menu button hovered and pressed images

---
+++ Assets/Scripts/ButtonStates/MM_PlayButton.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+// MM stands for MainMenu
+
+public class MM_PlayButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    private Button playButton;
+    [SerializeField] private Image givingHandImage;
+
+    // 0 for normal, 1 for hovered, 2 for pressed (pointer down and up)
+    [SerializeField] private List<Sprite> playButtonImages;
+
+    private void Awake()
+    {
+        // Find the play button inside the gameobject itself
+        playButton = GetComponent<Button>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        givingHandImage.sprite = playButtonImages[1]; // Change giving image to its hovered image
+
+        ((IPointerEnterHandler)playButton).OnPointerEnter(eventData);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        givingHandImage.sprite = playButtonImages[2]; // Change giving image to its pressed image
+
+        ((IPointerDownHandler)playButton).OnPointerDown(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        givingHandImage.sprite = playButtonImages[1]; // Change giving image to its hovered image
+
+        ((IPointerUpHandler)playButton).OnPointerUp(eventData);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        givingHandImage.sprite = playButtonImages[0]; // Change giving image to its normal image
+
+        ((IPointerExitHandler)playButton).OnPointerExit(eventData);
+    }
+}
---
+++ Assets/Scripts/ButtonStates/MM_QuitButton.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+// MM stands for MainMenu
+
+public class MM_QuitButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    private Button quitButton;
+    [SerializeField] private Image pointingImage;
+
+    // 0 for normal, 1 for hovered, 2 for pressed (pointer down and up)
+    [SerializeField] private List<Sprite> quitButtonImages;
+
+    private void Awake()
+    {
+        // Find the quit button inside the gameobject itself
+        quitButton = GetComponent<Button>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        pointingImage.sprite = quitButtonImages[1]; // Change pointing image to its hovered image
+
+        ((IPointerEnterHandler)quitButton).OnPointerEnter(eventData);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pointingImage.sprite = quitButtonImages[2]; // Change pointing image to its pressed image
+
+        ((IPointerDownHandler)quitButton).OnPointerDown(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pointingImage.sprite = quitButtonImages[1]; // Change pointing image to its hovered image
+
+        ((IPointerUpHandler)quitButton).OnPointerUp(eventData);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        pointingImage.sprite = quitButtonImages[0]; // Change pointing image to its normal image
+
+        ((IPointerExitHandler)quitButton).OnPointerExit(eventData);
+    }
+}
