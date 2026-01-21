4aeb118992956c7e4ab58f3c11be5085d4d40c9c
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2026-01-20 19:04:57 -0500

    PlayerUpgrades class and Player singleton

---
+++ Assets/Scripts/Player/PlayerUpgrades.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerUpgrades : MonoBehaviour
+{
+    Upgrades current;
+
+    public Upgrades Current => current;
+
+    public bool Has(Upgrades upgrade) => current.HasFlag(upgrade);
+    public void Give(Upgrades upgrade) => current |= upgrade;
+    public void Remove(Upgrades upgrade) => current &= ~upgrade;
+}
+
+[System.Flags]
+public enum Upgrades
+{
+    None = 0,
+    DoubleJump = 1 << 0,
+    Dash = 1 << 1,
+}
