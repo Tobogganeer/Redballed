e716506b11598faa62c8d93ac305f4f0187f0f8b
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2026-01-20 19:23:29 -0500

    Removed camera controller boundaries

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -13,44 +13,18 @@ public class CameraController : MonoBehaviour
     }
 
     public Transform target;
-    public Tilemap tilemap;
     public Vector2 offset;
     public Camera followCamera;
 
     [Space]
     public float followSmoothing = 2f;
 
-    Vector2 viewportHalfSize;
-
-    float leftBoundary;
-    float rightBoundary;
-    float bottomBoundary;
-
     Vector3 shakeOffset;
-
-    void Start()
-    {
-        // Recalculate bounds
-        tilemap.CompressBounds();
-        CalculateBounds();
-    }
-
-    private void CalculateBounds()
-    {
-        viewportHalfSize = new Vector2(followCamera.aspect * followCamera.orthographicSize, followCamera.orthographicSize);
-
-        leftBoundary = tilemap.transform.position.x + (tilemap.cellBounds.min.x * tilemap.transform.localScale.x) + viewportHalfSize.x;
-        rightBoundary = tilemap.transform.position.x + (tilemap.cellBounds.max.x * tilemap.transform.localScale.x) - viewportHalfSize.x;
-        bottomBoundary = tilemap.transform.position.y + (tilemap.cellBounds.min.y * tilemap.transform.localScale.y) + viewportHalfSize.y;
-    }
 
     private void LateUpdate()
     {
         Vector3 desiredPosition = target.position + new Vector3(offset.x, offset.y, transform.position.z) + shakeOffset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, 1f - Mathf.Exp(-followSmoothing * Time.deltaTime));
-
-        smoothedPosition.x = Mathf.Clamp(smoothedPosition.x, leftBoundary, rightBoundary);
-        smoothedPosition.y = Mathf.Clamp(smoothedPosition.y, bottomBoundary, smoothedPosition.y);
 
         transform.position = smoothedPosition;
     }
@@ -62,6 +36,7 @@ public class CameraController : MonoBehaviour
 
     private IEnumerator ShakeCoroutine(float intensity, float duration)
     {
+        // TODO: Better shake
         float timer = 0f;
         while (timer < duration)
         {
