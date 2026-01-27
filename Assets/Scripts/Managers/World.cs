d32263932b0510304ccd9705e82f1ff49c20b840
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2026-01-27 18:29:44 -0500

    Start of World to manage red balls

---
+++ Assets/Scripts/Managers/World.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    private static World instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    [SerializeField] int currentRedBalls;
+
+    public static int CurrentRedBalls => instance.currentRedBalls;
+
+    public static event Action<int> OnRedBallCollected;
+
+    public static void AddRedBalls(int num)
+    {
+        instance.currentRedBalls += num;
+        OnRedBallCollected?.Invoke(CurrentRedBalls);
+    }
+}
