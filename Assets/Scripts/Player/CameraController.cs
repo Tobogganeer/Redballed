6b111186383ce8b0513a33557bc65b0f343b750e
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2026-01-27 17:57:39 -0500

    Initial pass at camera predicting velocity

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Tobo.Util;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -15,13 +16,27 @@ public class CameraController : MonoBehaviour
     public Camera followCamera;
 
     [Space]
+    public Vector2 velocityPrediction = new Vector2(1.0f, 0.2f);
+    public Vector2 maxVelocityPrediction = new Vector2(5.0f, 2.0f);
+    public float velocitySmoothing = 0.4f;
+
+    [Space]
     public float followSmoothing = 2f;
 
     Vector3 shakeOffset;
+    Vector3 velocity;
 
     private void LateUpdate()
     {
+        Vector3 playerVelocity = Player.Movement.ActualVelocity;
+        playerVelocity *= velocityPrediction;
+        playerVelocity.x = Mathf.Clamp(playerVelocity.x, -maxVelocityPrediction.x, maxVelocityPrediction.x);
+        playerVelocity.y = Mathf.Clamp(playerVelocity.y, -maxVelocityPrediction.y, maxVelocityPrediction.y);
+
+        velocity = Vector3.Lerp(velocity, playerVelocity, Time.deltaTime * velocitySmoothing);
+
         Vector3 desiredPosition = target.position + new Vector3(offset.x, offset.y, transform.position.z) + shakeOffset;
+        desiredPosition -= velocity;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, 1f - Mathf.Exp(-followSmoothing * Time.deltaTime));
 
         transform.position = smoothedPosition;
