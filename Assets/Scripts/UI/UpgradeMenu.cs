62b760f7e60830f0730c2f761513354e249d9b07
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2026-02-09 14:30:24 -0500

    Finished upgrade scene UI

--- Assets/Scripts/UI/UpgradeMenu.cs
+++ Assets/Scripts/UI/UpgradeMenu.cs
@@ -1,23 +1,44 @@
 using UnityEngine;
+using TMPro;
 
 public class UpgradeMenu : MonoBehaviour
 {
-    public TMPro.TMP_Text redBallCountText;
-    public TMPro.TMP_Text nextDayText;
+    public TMP_Text redBallCountText;
+    public TMP_Text nextDayText;
+    public TMP_Text[] doubleJumpCostTexts;
+    public TMP_Text[] dashCostTexts;
 
-    private void OnEnable()
-    {
-        DayManager.OnInterdaySceneLoaded += OnLoaded;
-    }
+    [Space]
+    public GameObject doubleJumpUnlocked;
+    public GameObject doubleJumpLocked;
+    public GameObject dashUnlocked;
+    public GameObject dashLocked;
 
-    private void OnLoaded()
-    {
-        redBallCountText.text = World.CurrentRedBalls.ToString();
-        nextDayText.text = World.DayManager.CurrentDay.GetDayString();
-    }
+    [Space]
+    public int doubleJumpCost = 10;
+    public int dashCost = 20;
 
-    private void OnDisable()
+    private void Start()
     {
-        DayManager.OnInterdaySceneLoaded -= OnLoaded;
+        int redBalls = World.CurrentRedBalls;
+        redBallCountText.text = redBalls.ToString();
+        nextDayText.text = World.DayManager.CurrentDay.GetNextDay().GetDayString();
+
+        foreach (TMP_Text text in doubleJumpCostTexts)
+            text.text = doubleJumpCost.ToString();
+
+        foreach (TMP_Text text in dashCostTexts)
+            text.text = dashCost.ToString();
+
+        bool hasDoubleJump = redBalls >= doubleJumpCost;
+        bool hasDash = redBalls >= dashCost;
+
+        // Show proper UI depending on whether the upgrade is unlocked or not
+        doubleJumpUnlocked.SetActive(hasDoubleJump);
+        doubleJumpLocked.SetActive(!hasDoubleJump);
+        dashUnlocked.SetActive(hasDash);
+        dashLocked.SetActive(!hasDash);
+
+        // TODO: Set these values for the current player
     }
 }
