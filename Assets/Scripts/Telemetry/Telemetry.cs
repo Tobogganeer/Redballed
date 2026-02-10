7809e44e3744f380c0ebb396762b8851581c12bc
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2026-02-10 15:05:31 -0500

    Log telemetry to file

--- Assets/Scripts/Telemetry/Telemetry.cs
+++ Assets/Scripts/Telemetry/Telemetry.cs
@@ -1,11 +1,38 @@
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 
 public class Telemetry : MonoBehaviour
 {
+    public bool log = true;
+    public bool logInEditor = false;
     public int playerStatesPerSecond = 10;
 
+    static List<string> outputBuffer = new List<string>();
+    float outputFlushInterval = 5f;
+    float flushTimer;
+    string outputPath;
+    int sessionID;
+
     float playerStateTimer;
+
+    private void Start()
+    {
+        if (!log || (!logInEditor && Application.isEditor))
+        {
+            Destroy(this);
+            return;
+        }
+
+        sessionID = Random.Range(10000, 99999);
+        outputPath = Path.Combine(Application.persistentDataPath, $"telemetry_{sessionID}.txt");
+
+        outputBuffer.Clear();
+
+        Log("SessionID", sessionID.ToString());
+        Log("Date", System.DateTime.Now.ToString());
+    }
 
     private void Update()
     {
@@ -17,16 +44,39 @@ public class Telemetry : MonoBehaviour
             if (Player.Exists && Player.Movement.Snapshots.Count > 0)
                 LogRaw(new PlayerTelemetryEntry("PlayerState", GetLatestSnapshot()));
         }
+
+        // This one counts up so we don't flush immediately
+        flushTimer += Time.deltaTime;
+        if (flushTimer > outputFlushInterval)
+        {
+            flushTimer = 0f;
+            FlushOutput();
+        }
+    }
+
+    void FlushOutput()
+    {
+        if (outputBuffer.Count == 0)
+            return;
+
+        //File.AppendAllLinesAsync(outputPath, outputBuffer);
+        File.AppendAllLines(outputPath, outputBuffer);
+        outputBuffer.Clear();
+    }
+
+    void OnApplicationQuit()
+    {
+        FlushOutput();
     }
 
     public static void Log(string type, string value = "")
     {
-        Debug.Log(JsonUtility.ToJson(new TelemetryEntry(type, value)));
+        outputBuffer.Add(JsonUtility.ToJson(new TelemetryEntry(type, value)));
     }
 
     public static void LogRaw(object obj)
     {
-        Debug.Log(JsonUtility.ToJson(obj));
+        outputBuffer.Add(JsonUtility.ToJson(obj));
     }
 
     PlayerController.Snapshot GetLatestSnapshot()
