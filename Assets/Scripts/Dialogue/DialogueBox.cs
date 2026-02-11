aca4f5241149a340807d861e006360e2622edb1a
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2026-02-11 15:24:56 -0500

    Typewriter effect for dialogue boxes

--- Assets/Scripts/Dialogue/DialogueBox.cs
+++ Assets/Scripts/Dialogue/DialogueBox.cs
@@ -7,21 +7,52 @@ public class DialogueBox : MonoBehaviour
     public Vector2 padding = new Vector2(0.3f, 0.3f);
     public Vector2 minSize = new Vector2(0.4f, 0.2f);
 
+    [Space]
+    public float charactersPerSecond = 30f;
+
+    string targetText;
+    int currentCharacters;
+    float textTimer;
+    Vector2 currentTextSize;
+
+    private void Start()
+    {
+        targetText = string.Empty;
+    }
 
     void Update()
     {
+        // Typewriter effect
+        if (currentCharacters < targetText.Length)
+        {
+            textTimer += Time.deltaTime;
+            if (textTimer > 1f / charactersPerSecond)
+            {
+                textTimer = 0f;
+                currentCharacters++;
+                // Update visible string and cache its size
+                text.text = targetText.Substring(0, currentCharacters);
+                currentTextSize = text.GetRenderedValues();
+            }
+        }
+
         Vector2 size = Vector2.zero;
         if (text.text.Length > 0)
         {
-            Vector2 textSize = text.GetRenderedValues();
-
             // Getting weird, massive size for the first frame - clamp it
-            if (textSize.sqrMagnitude < 1000f * 1000f)
-                size = textSize;
+            if (currentTextSize.sqrMagnitude < 1000f * 1000f)
+                size = currentTextSize;
         }
 
         Vector2 finalSize = new Vector2(Mathf.Max(size.x, minSize.x), Mathf.Max(size.y, minSize.y));
 
         background.size = finalSize + padding;
+    }
+
+    public void SetText(string text, bool updateInstantly = false)
+    {
+        targetText = text;
+        currentCharacters = updateInstantly ? targetText.Length : 0;
+        textTimer = 0f;
     }
 }
