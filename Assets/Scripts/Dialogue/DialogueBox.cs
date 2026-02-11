721fe94fa8925c75cf0e12030f4e9a47c3c90ace
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2026-02-11 15:19:03 -0500

    Make dialogue box fit text

--- Assets/Scripts/Dialogue/DialogueBox.cs
+++ Assets/Scripts/Dialogue/DialogueBox.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class DialogueBox : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public TMPro.TMP_Text text;
+    public SpriteRenderer background;
+    public Vector2 padding = new Vector2(0.3f, 0.3f);
+    public Vector2 minSize = new Vector2(0.4f, 0.2f);
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        Vector2 size = Vector2.zero;
+        if (text.text.Length > 0)
+        {
+            Vector2 textSize = text.GetRenderedValues();
+
+            // Getting weird, massive size for the first frame - clamp it
+            if (textSize.sqrMagnitude < 1000f * 1000f)
+                size = textSize;
+        }
+
+        Vector2 finalSize = new Vector2(Mathf.Max(size.x, minSize.x), Mathf.Max(size.y, minSize.y));
+
+        background.size = finalSize + padding;
     }
 }
