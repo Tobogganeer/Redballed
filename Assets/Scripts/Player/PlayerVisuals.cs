71a8e3cbedbdaa1ae287cd492e25b546ba5082ac
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2026-02-14 17:31:29 -0500

    Hooked up backend to animations

--- Assets/Scripts/Player/PlayerVisuals.cs
+++ Assets/Scripts/Player/PlayerVisuals.cs
@@ -8,9 +8,11 @@ public class PlayerVisuals : MonoBehaviour
     public SpriteRenderer bodyRenderer;
     public PlayerController playerController;
 
-    private int isWalkingHash = Animator.StringToHash("IsWalking");
-    private int isGroundedHash = Animator.StringToHash("IsGrounded");
-    private int isDeadHash = Animator.StringToHash("IsDead");
+    private readonly int MovingHash = Animator.StringToHash("moving");
+    private readonly int GroundedHash = Animator.StringToHash("grounded");
+    private readonly int SlideHash = Animator.StringToHash("slide");
+    private readonly int DashHash = Animator.StringToHash("dash");
+    private readonly int JumpHash = Animator.StringToHash("jump");
 
     // Essentially an epsilon
     const float MinVelocityForWalkingAnim = 0.01f;
@@ -20,7 +22,7 @@ public class PlayerVisuals : MonoBehaviour
         VisualsUpdate();
     }
 
-    public bool IsWalking()
+    public bool IsMoving()
     {
         // Make sure we are trying to move and not up against a wall
         bool tryingToMove = playerController.PlayerInput != 0f;
@@ -30,13 +32,36 @@ public class PlayerVisuals : MonoBehaviour
 
     private void VisualsUpdate()
     {
-        animator.SetBool(isWalkingHash, IsWalking());
-        animator.SetBool(isGroundedHash, playerController.Grounded);
+        animator.SetBool(MovingHash, IsMoving());
+        animator.SetBool(GroundedHash, playerController.Grounded);
+        // TODO: Slide (when pushing up against a wall)
 
         //if (playerController.IsDead())
         //    animator.SetTrigger(isDeadHash);
 
         // Facing right by default - flip the X if we are facing left
         bodyRenderer.flipX = playerController.Facing == PlayerController.FacingDirection.Left;
+    }
+
+    private void OnEnable()
+    {
+        playerController.OnDash += PlayerController_OnDash;
+        playerController.OnJump += PlayerController_OnJump;
+    }
+
+    private void PlayerController_OnJump()
+    {
+        animator.SetTrigger(JumpHash);
+    }
+
+    private void PlayerController_OnDash()
+    {
+        animator.SetTrigger(DashHash);
+    }
+
+    private void OnDisable()
+    {
+        playerController.OnDash -= PlayerController_OnDash;
+        playerController.OnJump -= PlayerController_OnJump;
     }
 }
