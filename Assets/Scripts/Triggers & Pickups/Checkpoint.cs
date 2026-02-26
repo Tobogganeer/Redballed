a8e67755e4b32c2945a8f60b8483c64295208d4b
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2026-02-26 14:20:59 -0500

    Add "starting" checkpoint for a level

--- Assets/Scripts/Triggers & Pickups/Checkpoint.cs
+++ Assets/Scripts/Triggers & Pickups/Checkpoint.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class Checkpoint : Trigger
 {
+    public bool startingCheckpoint;
+
     static Checkpoint lastCheckpoint;
 
     public static bool Exists => lastCheckpoint != null;
-    public static Checkpoint Current => Exists ? lastCheckpoint : null;
+    public static Checkpoint Current => Exists ? lastCheckpoint : throw new System.NullReferenceException("No starting checkpoint in level!");
+    public static Vector3 CurrentRespawnPoint => Current.transform.position;
+
+    private void Start()
+    {
+        if (startingCheckpoint)
+            lastCheckpoint = this;
+    }
 
     protected override void OnPlayerEnter()
     {
