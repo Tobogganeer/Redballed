879b9cf65f3b5740a9cbad7826d883cf67e28137
Osama Hussein <osamahusseinn2@gmail.com>
2026-03-26 23:21:08 -0400

    Fixed run away ending after pressing the continue button

--- Assets/Scripts/Ending.cs
+++ Assets/Scripts/Ending.cs
@@ -8,6 +8,13 @@ using UnityEngine.UI;
 
 public class Ending : MonoBehaviour
 {
+    private enum Endings
+    {
+        None,
+        Ending1,
+        Ending2
+    };
+
     [Header("Button objects")]
     [SerializeField] private Button enlistButton;
     [SerializeField] private Button runAwayButton;
@@ -21,11 +28,13 @@ public class Ending : MonoBehaviour
     [SerializeField] private List<string> ending2Texts;
 
     int endingLineIndex = 0;
+    Endings currentEnding = Endings.None;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if (endingLineIndex != 0) endingLineIndex = 0;
+        if (currentEnding != Endings.None) currentEnding = Endings.None;
         if (endingTextObject != null) endingTextObject.text = firstEndingText;
         if (enlistButton != null) enlistButton.gameObject.SetActive(true);
         if (runAwayButton != null) runAwayButton.gameObject.SetActive(true);
@@ -47,6 +56,8 @@ public class Ending : MonoBehaviour
             if (enlistButton != null) Destroy(enlistButton.gameObject);
             if (runAwayButton != null) Destroy(runAwayButton.gameObject);
             if (continueButton != null) continueButton.gameObject.SetActive(true);
+
+            currentEnding = Endings.Ending1;
         }
     }
 
@@ -59,15 +70,49 @@ public class Ending : MonoBehaviour
             if (enlistButton != null) Destroy(enlistButton.gameObject);
             if (runAwayButton != null) Destroy(runAwayButton.gameObject);
             if (continueButton != null) continueButton.gameObject.SetActive(true);
+
+            currentEnding = Endings.Ending2;
         }
     }
 
     public void ContinueEndingLine()
     {
-        endingLineIndex++;
+        switch (currentEnding)
+        {
+            case Endings.Ending1: // Enlist lines
 
-        if (endingTextObject != null) endingTextObject.text = ending1Texts[endingLineIndex];
+                endingLineIndex++;
 
-        if (endingLineIndex >= ending1Texts.Count - 1 && continueButton != null) Destroy(continueButton.gameObject);
+                if (endingTextObject != null && ending1Texts[endingLineIndex] != null)
+                {
+                    endingTextObject.text = ending1Texts[endingLineIndex];
+                }
+
+                if (endingLineIndex >= ending1Texts.Count - 1 && continueButton != null)
+                {
+                    Destroy(continueButton.gameObject);
+                }
+
+                break;
+
+                case Endings.Ending2: // Run away lines
+
+                endingLineIndex++;
+
+                if (endingTextObject != null && ending2Texts[endingLineIndex] != null)
+                {
+                    endingTextObject.text = ending2Texts[endingLineIndex];
+                }
+
+                if (endingLineIndex >= ending2Texts.Count - 1 && continueButton != null)
+                {
+                    Destroy(continueButton.gameObject);
+                }
+
+                break;
+
+            default:
+                break;
+        }
     }
 }
